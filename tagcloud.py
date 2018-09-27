e0fd4238c09a2189d2713dc10ab19808b719ffe7
spio <spio@tiguriner>
2018-09-27 12:06:25 +0200

    - automating the allpdfs generation

--- tagcloud.py
+++ tagcloud.py
@@ -11,3 +11,5 @@ os.system("rm pdfs/*.txt")
 
 print("Written tag cloud source to _tagcloud.txt.")
 print("Render with https://www.wordclouds.com/.")
+
+os.system("pdftk pdfs/*.pdf cat output _allpdfs.pdf")
