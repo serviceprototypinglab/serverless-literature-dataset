180b197838d8354b7d75403b78e199419b969e6c
spio <spio@tiguriner>
2019-04-22 09:52:37 +0200

    - systematic tracking of negatives to make follow-up analysis more robust

--- scraper/parsedblp.py
+++ scraper/parsedblp.py
@@ -4,6 +4,7 @@ import json
 
 base_filename = "../serverless-literature-base.json"
 biblio_filename = "../serverless-literature-bibliography.json"
+neg_filename = "negative.json"
 
 # Authors,Title,(Type),Where,Year
 
@@ -21,6 +22,20 @@ literature = json.load(f)
 
 f = open(biblio_filename)
 biblio = json.load(f)
+
+f = open(neg_filename)
+negs = json.load(f)
+
+neglinks = []
+for neg in negs:
+	if "doi" in neg:
+		neglinks.append(neg["doi"])
+	elif "usenix" in neg:
+		neglinks.append(neg["usenix"])
+	elif "arxiv" in neg:
+		neglinks.append(neg["arxiv"])
+	elif "link" in neg:
+		neglinks.append(neg["link"])
 
 nextnum = sorted([int(x) for x in literature])[-1] + 1
 courtesy = False
@@ -43,6 +58,9 @@ for searchterm in searchterms:
 		title = r["Title"]
 		skip = False
 		skipped = ""
+		if doi in neglinks:
+			skip = True
+			skipped = "(negative)"
 		for ident in literature:
 			if "doi" in literature[ident] and literature[ident]["doi"] == doi:
 				skip = True
@@ -51,6 +69,9 @@ for searchterm in searchterms:
 				skip = True
 				skipped = "(present)"
 			elif "usenix" in literature[ident] and literature[ident]["usenix"] == doi:
+				skip = True
+				skipped = "(present)"
+			elif "link" in literature[ident] and literature[ident]["link"] == doi:
 				skip = True
 				skipped = "(present)"
 		if not skip:
@@ -63,8 +84,11 @@ for searchterm in searchterms:
 					literature[skipped] = {"arxiv": doi}
 				elif "usenix.org" in doi:
 					literature[skipped] = {"usenix": doi}
+				else:
+					literature[skipped] = {"link": doi}
 				nextnum += 1
 			else:
+				neglinks.append(doi)
 				skipped = "(excluded)"
 		doiactions[doi] = skipped
 
@@ -81,4 +105,19 @@ for searchterm in searchterms:
 	f = open(base_filename, "w")
 	base_sorted = {int(x) : literature[x] for x in literature}
 	json.dump(base_sorted, f, indent=2, ensure_ascii=False, sort_keys=True)
+	f.close()
+
+	negs = []
+	for doi in neglinks:
+		if "doi.org" in doi:
+			negs.append({"doi": doi})
+		elif "arxiv.org" in doi:
+			negs.append({"arxiv": doi})
+		elif "usenix.org" in doi:
+			negs.append({"usenix": doi})
+		else:
+			negs.append({"link": doi})
+
+	f = open(neg_filename, "w")
+	json.dump(negs, f, indent=2, ensure_ascii=False, sort_keys=True)
 	f.close()
